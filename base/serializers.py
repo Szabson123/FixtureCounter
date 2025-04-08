277b7e3b8cd92a98237615a767a0f6de79afee5d
Szabson123 <szymonzaba18@gmail.com>
2025-04-08 10:56:50 +0200

    checkpoint before clear counter

--- base/serializers.py
+++ base/serializers.py
@@ -35,11 +35,19 @@ class FullInfoFixtureSerializer(serializers.ModelSerializer):
     counter_all = serializers.IntegerField(source='counter_all.counter', read_only=True)
     counter_last_maint = serializers.IntegerField(source='counter_last_maint.counter', read_only=True)
     last_maint_date = serializers.SerializerMethodField()
+    limit_procent = serializers.SerializerMethodField()
 
     class Meta:
         model = Fixture
-        fields = ['name', 'counter_all', 'counter_last_maint', 'last_maint_date']
+        fields = ['id', 'name', 'counter_all', 'counter_last_maint', 'last_maint_date', 'limit_procent']
 
     def get_last_maint_date(self, obj):
         last_counter = obj.backup.order_by('-date').first()
         return last_counter.date if last_counter else None
+    
+    def get_limit_procent(self, obj):
+        counter_value = getattr(obj.counter_last_maint, 'counter', None)
+        if counter_value is not None and counter_value != 0:
+            return round((counter_value / obj.cycles_limit) * 100, 2)
+        return None
+    
