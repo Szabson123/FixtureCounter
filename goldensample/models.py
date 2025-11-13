e7a12f4fb697ff9fbe81c97dc392456affd8c528
Szabson123 <149076057+Szabson123@users.noreply.github.com>
2025-11-13 13:05:24 +0100

    Check golden for FWK

--- goldensample/models.py
+++ goldensample/models.py
@@ -31,6 +31,7 @@ class ProcessName(models.Model):
 class TypeName(models.Model):
     name = models.CharField(max_length=255)
     color = models.CharField(max_length=255, null=True, blank=True)
+    compute_name = models.CharField(max_length=255, null=True, blank=True)
     
     def __str__(self):
         return self.name
@@ -63,7 +64,7 @@ class MasterSample(models.Model):
 
     # endcodes -> multi
     project_name = models.CharField(max_length=255)
-    sn = models.CharField(max_length=255)
+    sn = models.CharField(max_length=255, unique=True)
     date_created = models.DateField(auto_now_add=True)
     expire_date = models.DateField()
     pcb_rev_code = models.CharField(max_length=255)
@@ -76,3 +77,46 @@ class MasterSample(models.Model):
 class MachineGoldensTime(models.Model):
     machine_name = models.CharField(max_length=255, unique=True)
     date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.machine_name
+
+
+class EndCodeTimeFWK(models.Model):
+    machine_id = models.CharField(max_length=255)
+    site = models.PositiveIntegerField()
+    last_good_tested = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.machine_id
+
+
+class LastResultFWK(models.Model): #remember its not sn to its result 
+    sn = models.CharField(max_length=255)
+    result = models.CharField(max_length=255, null=True, blank=True)
+    date_time_tested = models.DateTimeField(auto_now_add=True)
+    machine_id = models.CharField(max_length=255)
+    site = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.machine_id} site {self.site}'
+
+
+class TempCheckMasterFWK(models.Model):
+    machine_id = models.CharField(max_length=255)
+    site = models.PositiveIntegerField()
+    pass_res = models.BooleanField(null=True)
+    fail_res = models.BooleanField(null=True)
+
+    def __str__(self):
+        return f'{self.machine_id} site {self.site}'
+
+
+class TempMasterShow(models.Model):
+    machine_id = models.CharField(max_length=255)
+    site = models.PositiveIntegerField()
+    if_set = models.BooleanField(default=False)
+    sn = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.machine_id} site {self.site}'
