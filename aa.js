a5f489b5d29dcc5193665e783fd43f7c69319066
Szabson123 <149076057+Szabson123@users.noreply.github.com>
2025-12-15 13:59:52 +0100

    prepare for admin pages

--- aa.js
+++
@@ -1,6 +0,0 @@
-const interval = setInterval(() => {
-  const inputs = document.querySelectorAll('td[role="gridcell"] input[type="text"][maxlength="20"]');
-  if (inputs.length > 0) {
-    inputs.forEach(i => i.maxLength = 50);
-  }
-}, 300);
