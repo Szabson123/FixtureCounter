b0bc87b506b8359b1bd6ee33c347db24612321ba
Szabson123 <149076057+Szabson123@users.noreply.github.com>
2026-01-31 15:18:17 +0100

    filter

--- spea_control/filters.py
+++ spea_control/filters.py
@@ -3,18 +3,24 @@ from .models import SpeaCard
 
 class SpeaCardFilter(django_filters.FilterSet):
     is_main_wardrobe = django_filters.BooleanFilter(method='filter_is_main_wardrobe')
+    is_out_of_company = django_filters.BooleanFilter(method='filter_is_out_of_company')
 
     class Meta:
         model = SpeaCard
-        fields = ['category', 'is_broken', 'sn']
+        fields = ['category', 'is_broken', 'sn', 'is_out_of_company']
 
     def filter_is_main_wardrobe(self, queryset, name, value):
-        """
-        value: True lub False (przekazane w URL)
-        """
         if value is True:
             return queryset.filter(location__name='Szafa')
         elif value is False:
             return queryset.exclude(location__name='Szafa')
+        
+        return queryset
+    
+    def filter_is_out_of_company(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(out_of_company=True)
+        if value is False:
+            return queryset.filter(out_of_company=False)
         
         return queryset
