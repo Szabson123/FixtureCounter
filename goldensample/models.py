7ce42138cd25d984586463d21a6908a7ecc43b9f
Szabson <szymonzaba18@gmail.com>
2026-05-15 10:14:09 +0200

    new admin

--- goldensample/models.py
+++ goldensample/models.py
@@ -64,6 +64,9 @@ class Department(models.Model):
 
 class AdditionalNameProject(models.Model):
     name = models.CharField(max_length=255) 
+    
+    def __str__(self):
+        return self.name
 
 
 class EndCode(models.Model):
