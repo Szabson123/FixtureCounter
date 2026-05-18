1a6657bbf9d1cdfbba05a60e74dea4b2512411dd
Szabson <szymonzaba18@gmail.com>
2026-05-18 11:10:37 +0200

    try hot fix

--- goldensample/filters.py
+++ goldensample/filters.py
@@ -7,6 +7,10 @@ class MasterSampleFilter(django_filters.FilterSet):
     master_type = django_filters.BaseInFilter(field_name="master_type", lookup_expr="in")
     departament = django_filters.BaseInFilter(field_name="departament", lookup_expr="in")
     additional_project_name = django_filters.BaseInFilter(field_name="additional_project_name", lookup_expr="in")
+    
+    endcodes = django_filters.BaseInFilter(field_name="endcodes__code", lookup_expr="in", distinct=True)
+    code_smd = django_filters.BaseInFilter(field_name="code_smd__code", lookup_expr="in", distinct=True)
+
     class Meta:
         model = MasterSample
-        fields = ['client', 'process_name', 'master_type', 'departament', 'additional_project_name']
+        fields = ['client', 'process_name', 'master_type', 'departament', 'additional_project_name', 'endcodes', 'code_smd']
