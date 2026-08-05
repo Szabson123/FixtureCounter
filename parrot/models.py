9ffb07b6356136b52718a409af2385c6748d5fda
Szabson <szymonzaba18@gmail.com>
2026-08-05 11:16:16 +0200

    Parrot3

--- parrot/models.py
+++ parrot/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+import uuid
 
+def get_uuid():
+    return uuid.uuid4
 
 class TestedParrotSn(models.Model):
     sn = models.CharField(max_length=255, db_index=True, unique=True)
@@ -7,9 +10,11 @@ class TestedParrotSn(models.Model):
     done = models.BooleanField()
     error = models.TextField(null=True, blank=True)
     internal_code = models.CharField(max_length=255, db_index=True)
+    certyficate = models.CharField(max_length=255, unique=True, db_index=True, null=True, blank=True)
 
     class Meta:
         indexes = [
             models.Index(fields=['date', 'id', 'sn'], name='parrot_date_id_sn_idx'),
             models.Index(fields=['date', 'done', 'internal_code'], name='parrot_date_done_ic_idx'),
+            models.Index(fields=['certyficate', 'done'], name='parrot_cert_done_idx'),
         ]
