14df89eb7fabc23cc0ccb7f2073180b0cc7d15c5
Szabson <szymonzaba18@gmail.com>
2026-09-10 10:29:47 +0200

    admin update

--- aidon/admin.py
+++ aidon/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+@admin.register(SnToBoard)
+class SnToBoardAdmin(admin.ModelAdmin):
+    list_display = (
+        'sn',
+        'pallet',
+        'place_num',
+        'aoi_result',
+        'ict_result',
+        'fvt_result',
+        'full_result',
+    )
+    list_filter = ('full_result', 'pallet')
+    search_fields = ('sn',)
 
-admin.site.register(SnToBoard)
-admin.site.register(PalletFullInfo)
+@admin.register(PalletFullInfo)
+class PalletFullInfoAdmin(admin.ModelAdmin):
+    list_display = ('pallet_number', 'db_board_id', 'full_used', 'created_at')
